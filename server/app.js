5555ac9ab66c3fe88b1a126f4d463015fd41cff7
Habib <auduhabib1990@gmail.com>
2017-10-25 09:24:50 +0100

    deleted bin folder starting my server through app.js,modify app.js to start my server,modify package.js start script

--- server/app.js
+++ server/app.js
@@ -1,31 +1,45 @@
 import express from 'express';
-
+import http from 'http';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-// express app set up
+/**
+ * set up app server
+ */
 
 const app = express();
 
-// log request to the console
+/**
+ * log request to the console
+ */
 
 app.use(logger('dev'));
 
-// parsing incomming request data
+/**
+ * Parsing incomming request data
+ */
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-require('./server/routes')(app);
-
 /**
- * 
+ * created my server using http.createServer, and passing the express server "app" as 
+ * parameter
+ * set port to 4000
  */
+const port = parseInt(process.env.PORT, 10) || 4000;
+app.set('port', port);
 
+const server = http.createServer(app);
+server.listen(port);
 
-app.get('*', (req, res) => res.status(200).send({ message: 'Welcome to our More-Recipes Api',
-}));
+// require('./server/routes')(app);
+
+app.get('*', (req, res) => res.status(200).send({ message: 'Welcome to My charting Api',
+})
+
+);
 
 export default app;
 
